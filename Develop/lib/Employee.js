e3e29b30bcd8d7d4833cef7884623944637b7a99
Miller Rich <william.miller.rich@gmail.com>
2020-09-15 21:44:03 -0500

    define and export employee class, engineer class, intern class, and manager class, npm run test all tests pass

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,50 @@
 // TODO: Write code to define and export the Employee class
+const { prompt } = require("inquirer");
+
+class Employee {
+    constructor(name, id, email) {
+    this.name = name;
+    this.id = id;
+    this.email = email;
+    }
+    getName() {
+        return this.name;
+    }
+    getId() {
+        return this.id;
+    }
+    getEmail() {
+        return this.email;
+    }
+    getRole() {
+        return "Employee";
+    }
+}
+
+module.exports = Employee;
+
+// const questions = [
+//     {
+//         type: "input",
+//         message: "What is the employee's name?",
+//         name: "name"
+//     },
+//     {
+//         type: "input",
+//         message: "What is the employee's id?",
+//         name: "id"
+//     },
+//     {
+//         type: "input",
+//         message: "What is the employee's email address?",
+//         name: "email"
+//     }
+// ]
+
+// init();
+// function init() {
+//     prompt(questions).then(input => {
+//         console.log(input);
+//     })
+    
+// }
